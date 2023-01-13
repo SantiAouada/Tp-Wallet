ce3105ab05a24dbb9500764a2c93b82f4e4cfb70
SantiAouada <santiago.aouada@gmail.com>
2023-01-13 12:36:30 -0300

    5

---
+++ ap.js
@@ -0,0 +1,16 @@
+let btningreso =document.getElementById('adingreso')
+
+btningreso.addEventListener ('click',anadiringreso)
+
+
+
+
+let ingreso1 = document.getElementById('ingreso')
+
+
+
+const anadiringreso=(event) =>{
+    ingreso.innerHTML
+}
+
+ingreso1.addEventListener('click',anadiringreso)
